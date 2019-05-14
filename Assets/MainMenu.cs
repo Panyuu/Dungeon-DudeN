09d5314eae0d9b891c01cbce0bcc7dca6369bd03
panyuu <tina.kiessling@t-online.de>
2019-05-15 00:43:28 +0200

    Menu added

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void continueGame() {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void newGame() {
+
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void exit() {
+
+
+        Debug.Log("QUIT!");
+        Application.Quit();
+
+    }
+
+}
